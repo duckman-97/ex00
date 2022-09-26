81e231a4cfaadb3e498724dafb2f7547975285df
duckman-97 <72430680+duckman-97@users.noreply.github.com>
2022-09-26 13:24:30 +0900

    first

---
+++ src/main/java/org/zerock/sample/SampleHotel.java
@@ -0,0 +1,20 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@Component
+@ToString
+@Getter
+@RequiredArgsConstructor
+public class SampleHotel {
+
+	@NonNull
+	private Chef chef;
+
+	private String name;
+}
